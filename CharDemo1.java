0cdb2c017a08712365a45ef534e4e7d14ed2d37c
gayatrivaishya <vaishyagayatri37@gmail.com>
2024-07-15 13:48:31 +0530

    Add files via upload

---
+++ CharDemo1.java
@@ -0,0 +1,9 @@
+class CharDemo1{
+public static void main (String args[])
+char ch1,ch2;
+ch1=88;
+ch2='y';
+System.out.print{"ch1 and ch2:");
+System.out.print(ch1 + " " + ch2);
+}
+}
---
+++ Example1.java
@@ -0,0 +1,7 @@
+
+class Example1 {
+// Your program begins with a call to main().
+public static void main(String args[]) {
+System.out.println("This is a simple Java program.");
+}
+}
